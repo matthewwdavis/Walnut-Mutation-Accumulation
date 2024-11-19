0590ead52b97cecd21c805190c70b2b14d59b9fd
matthewwdavis <102768051+matthewwdavis@users.noreply.github.com>
2024-11-19 15:04:15 -0800

    adding many script updates

--- Scripts/parsing_data_hpc.R
+++ Scripts/parsing_data_hpc.R
@@ -1,24 +1,13 @@
----
-title: "parsing_data"
-author: "Matthew Davis"
-date: "2024-10-31"
-output: html_document
----
-
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(echo = TRUE)
-```
-
 # Setup
 ## Libraries
-```{r, message=FALSE}
+
 library(polymorphology2)
 library(tidyverse)
 library(pbapply)
-```
+
 
 ## Functions
-```{r}
+
 mutations_embryo_tree<-function(mutations_sub){
   mutations_sub<-mutations_sub[Type=="Embryo"]
   mutations_matrix<-data.table(table(UNIQUE=mutations_sub$UNIQUE,
@@ -30,7 +19,6 @@ mutations_embryo_tree<-function(mutations_sub){
   clustplot<-plot(clust)
   return(list(dist=distance_mat, plot=clustplot))
 }
-
 
 annot_mut_effect<-function(i){
   genic <- mutations_means$CDS[i]
@@ -51,10 +39,10 @@ annot_mut_effect<-function(i){
   AA <- seqinr::translate(toupper(CDS[[mod]]))
   genome_BPs<-fasta[[CHROM]][CDS_POS]
   genome_AA<-seqinr::translate(toupper(genome_BPs))
-
+  
   REF_genome<-toupper(fasta[[CHROM]][POS])
   if(REF_genome!=REF) return("REF_genome_mismatch")
-
+  
   CDS_dt<-data.table(cPOS=CDS_POS, BP=toupper(cds[[mod]]))
   if(DIR=="-") {
     CDS_dt$cPOS<-rev(CDS_dt$cPOS)
@@ -73,7 +61,7 @@ annot_mut_effect<-function(i){
   SYN=all(CDS_dt$AA==CDS_dt$newAA)
   message(i, mod, CDS_dt[which(CDS_dt$cPOS==POS)]$AA," to ", CDS_dt[which(CDS_dt$cPOS==POS)]$newAA)
   message(ifelse( SYN, "\tSyn","\tNon-Syn"))
-
+  
   effect<-CDS_dt[which(CDS_dt$cPOS==POS)]
   effect$gene_model<-mod
   effect$CHROM<-CHROM
@@ -84,16 +72,14 @@ vstrsplit<-function(x,...){
   unlist(strsplit(x,...))
 }
 
-
-
 CDS_codon_mutation_table <- function(CDS) {
   # Precompute mutations table
   muts <- data.table(table(REF = c("A", "T", "C", "G"), ALT = c("A", "T", "C", "G")))[REF != ALT]
-
+  
   # Create codon table with all possible mutations
   codons <- data.table(SEQINR.UTIL$CODON.AA)
   codons[, CODON := toupper(CODON)]
-
+  
   # Precompute the CODON_mutations_table
   CODON_mutations_table <- rbindlist(lapply(1:nrow(codons), function(i) {
     CODON <- codons$CODON[i]
@@ -109,7 +95,7 @@ CDS_codon_mutation_table <- function(CDS) {
     CODON_dt[, effect := ifelse(REFAA == ALTAA, "S", "N")]
     return(CODON_dt)
   }))
-
+  
   # Convert each CDS into a mutation table
   pb <- txtProgressBar(min = 0, max = length(CDS), style = 3)
   CDS_dt_all <- rbindlist(lapply(1:length(CDS), function(i) {
@@ -119,22 +105,22 @@ CDS_codon_mutation_table <- function(CDS) {
       message("Gene not multiple of 3")
       return(NULL)
     }
-
+    
     original_protein <- seqinr::translate(random_seq)
     CDS_dt <- data.table(POS = rep(1:3, times = length(random_seq) / 3),
                          DNAPOS = 1:length(random_seq),
                          REF = random_seq,
                          AA = rep(original_protein, each = 3))
-
+    
     # Extract codon for each amino acid
     codons_list <- sapply(seq(1, length(random_seq), by = 3), function(start) {
       paste(random_seq[start:(start + 2)], collapse = "")
     })
     CDS_dt[, REFCODON := rep(codons_list, each = 3)]
-
+    
     # Merge with mutations to add ALT column of all possible mutations at each codon
     CDS_dt_mutations_table <- unique(merge(CDS_dt, muts, by = "REF", allow.cartesian = TRUE))[order(DNAPOS)]
-
+    
     # Merge with CODON_mutations_table to annotate effect
     CDS_dt_mutations_table <- merge(CDS_dt_mutations_table, CODON_mutations_table, by = c("REFCODON", "REF", "POS", "ALT"))
     setTxtProgressBar(pb, i)
@@ -142,10 +128,9 @@ CDS_codon_mutation_table <- function(CDS) {
     return(CDS_dt_mutations_table[, .(REF, ALT, effect, GENE)])
   }))
   close(pb)
-
+  
   return(CDS_dt_all)
 }
-
 
 mutation_probs<-function(muts, genome, cds=NULL){
   message("Calculating base pair frequencies from genome...")
@@ -156,27 +141,26 @@ mutation_probs<-function(muts, genome, cds=NULL){
     return(bp_freq)
   }))
   bp_freq<-bp_freq[REF %in% c("A","T","C","G"),.(N=sum(N)), by="REF"]
-
+  
   mutations<-data.table((table(REF=muts$REF, ALT=muts$ALT)))[REF!=ALT]
   mutations<-merge(mutations, bp_freq, by="REF")
   mutations$rawPROB=(mutations$N.x/(mutations$N.y))
-
+  
   if(!is.null(cds)){
     message("Calculating base pair frequencies from CDS...")
     bp_freqCDS<-rbindlist(lapply(1:length(cds), function(i){
-
+      
       bp_freq<-data.table(table(REF=cds[[i]]))[REF!="n"]
       bp_freq$REF<-toupper(bp_freq$REF)
       return(bp_freq)
     }))
     bp_freqCDS<-bp_freqCDS[REF %in% c("A","T","C","G"),.(CDSN=sum(N)), by="REF"]
-
+    
     mutations<-merge(mutations, bp_freqCDS, by="REF")
     mutations$rawPROB=(mutations$N.x/(mutations$N.y-mutations$CDSN))
   }
   return(mutations)
 }
-
 
 sample_neutrals<-function(neutrals, N, i){
   samples<-sapply(1:i, function(i){
@@ -189,7 +173,7 @@ GvsIG<-function(mutations_data, REGIONS){
   REGIONS$ID<-1:nrow(REGIONS)
   REGIONS_mut<-sites_in_features(REGIONS,mutations_data, mode = "counts")
   REGIONS$mut<-REGIONS_mut$counts[match(REGIONS$ID, REGIONS_mut$ID)]
-
+  
   REGIONS_means<-REGIONS[,.(mut=sum(mut), LENGTH=sum(MAPP_LENGTH2), mutrate=sum(mut)/sum(MAPP_LENGTH2)), by=REGION]
   chitest<-chisq.test(REGIONS_means[,2:3])
   ratio<-REGIONS_means[REGION=="GENE"]$mutrate/REGIONS_means[REGION=="IG"]$mutrate
@@ -205,24 +189,72 @@ my_minimal<-function(){
           legend.background = element_blank(),
           plot.title = element_text(hjust=0.5, size=6))
 }
-```
+
+annot_mut_effect<-function(i){
+  genic<-mutations_means$CDS[i]
+  if(!genic) return("nonCDS")
+  row<-mutations_means[i]
+  POS<-row$POS
+  REF<-row$REF
+  ALT<-row$ALT
+  CHROM=row$CHROM
+  overlap<-sites_in_features(cds_good, row, mode = "any")[any==T]
+  mod<-unique(cds_good[ID%in%overlap$ID]$Parent)[1]
+  cds_model<-cds_good[Parent%in%mod]
+  DIR<-CDS_model$DIRECTION[1]
+  CDS_POS<-unlist(lapply(1:nrow(CDS_model), function(j){
+    CDS_model$START[j]:CDS_model$STOP[j]
+  }))
+  BPs<-protein_cds[[mod]]
+  AA<-seqinr::translate(toupper(protein_cds[[mod]]))
+  genome_BPs<-fasta[[CHROM]][CDS_POS]
+  genome_AA<-seqinr::translate(toupper(genome_BPs))
+  
+  REF_genome<-toupper(fasta[[CHROM]][POS])
+  if(REF_genome!=REF) return("REF_genome_mismatch")
+  
+  CDS_dt<-data.table(cPOS=CDS_POS, BP=toupper(protein_cds[[mod]]))
+  if(DIR=="-") {
+    CDS_dt$cPOS<-rev(CDS_dt$cPOS)
+    REF<-revcomp(REF)
+    ALT<-revcomp(ALT)
+    genome_AA<-seqinr::translate(unlist(strsplit(revcomp(toupper(fasta[[CHROM]][CDS_POS])), split="")))
+  }
+  if(!all(genome_AA==AA)) return("REF_genome_mismatch_AA")
+  if(CDS_dt[cPOS==POS]$BP!=REF) return(CDS_dt[cPOS==POS]$BP)
+  CDS_dt$AA<-rep(seqinr::translate(CDS_dt$BP), each=3)
+  CDS_dt$BP2<-CDS_dt$BP
+  CDS_dt$BP2[which(CDS_dt$cPOS==POS)]<-ALT
+  CDS_dt[BP2!=BP]
+  CDS_dt$newAA<-rep(seqinr::translate(CDS_dt$BP2), each=3)
+  CDS_dt$AAPOS<-rep(1:length(seqinr::translate(CDS_dt$BP)), each=3)
+  SYN=all(CDS_dt$AA==CDS_dt$newAA)
+  message(i, mod, CDS_dt[which(CDS_dt$cPOS==POS)]$AA," to ", CDS_dt[which(CDS_dt$cPOS==POS)]$newAA)
+  message(ifelse( SYN, "\tSyn","\tNon-Syn"))
+  
+  effect<-CDS_dt[which(CDS_dt$cPOS==POS)]
+  effect$gene_model<-mod
+  effect$CHROM<-CHROM
+  ifelse(SYN, "Syn","Non-Syn")
+}
+
 
 # Genome and annotations
 ## Preparing data
 ### Read in data
-```{r}
+
 # Mappability
-mapp <- read.bedGraph("./Data/Genmap/K150E0/ref_chandler_primary_default_scaffold.genmap.bedgraph")
+mapp <- read.bedGraph("./ref_chandler_primary_default_scaffold.genmap.bedgraph")
 
 # Genome
-fasta <- read.fasta("./Data/Fastas/ref_chandler_primary_default_scaffold.fasta")
+fasta <- read.fasta("./ref_chandler_primary_default_scaffold.fasta")
 
 # Annotations
-gff <- read.GFF("./Data/GFFs/Chromosome_Only_Liftoff/ref_chandler_primary_default_scaffold_chr_only_liftoff_a99s99.gff")
-```
+gff <- read.GFF("./ref_chandler_primary_default_scaffold_chr_only_liftoff_a99s99.gff")
+
 
 ### Rename chromosomes
-```{r}
+
 gff$CHROM<-as.numeric(gsub("NC_0499(..).1_RagTag", "\\1", gff$CHROM))
 unique(gff$CHROM)
 
@@ -233,12 +265,10 @@ unique(gff$CHROM)
 names(fasta)<-as.numeric(gsub("NC_0499(..).1_RagTag", "\\1", names(fasta)))
 fasta <- fasta[!is.na(names(fasta))]
 unique(names(fasta))
-```
-
 
 ## Extract data
 ### Extract genes and cds sequences 
-```{r}
+
 genes <- gff %>%
   filter(TYPE %in% "gene")
 
@@ -260,10 +290,10 @@ sum(duplicated(cds$GENE))
 
 
 cds$ID <- 1:nrow(cds)
-```
+
 
 ### Protein amino acids
-```{r}
+
 proteins <- pblapply(unique(cds$Parent), function(p){
   cds_p <- cds[Parent == p]
   dir <- unique(cds_p$DIRECTION)
@@ -284,10 +314,9 @@ names(proteins) <- unique(cds$Parent)
 
 #proteins
 sum(duplicated(names(proteins)))
-```
 
 ### Extract protein DNA sequences
-```{r}
+
 protein_cds <- pblapply(unique(cds$Parent), function(p){
   cds_p <- cds[Parent == p]
   dir <- unique(cds_p$DIRECTION)
@@ -308,11 +337,10 @@ names(protein_cds)<-unique(cds$Parent)
 
 ##protein_cds
 sum(duplicated(names(proteins)))
-```
 
 ## Checking sequences
 ### Make sure protein sequences are correct (start with M, ends with *)
-```{r}
+
 prot_check <- rbindlist(pblapply(names(proteins), function(p){
   codon1 <- proteins[[p]][1]
   codonend <- proteins[[p]][length(proteins[[p]])]
@@ -322,10 +350,9 @@ fill=T)
 
 prot_check$CHECK <- prot_check$codon1 == "M" & prot_check$codonend == "*"
 table(prot_check$CHECK)
-```
 
 ### Only keep valid proteins in annotations
-```{r}
+
 cds_good <- cds[Parent %in% prot_check[CHECK==T]$p]
 genes_good <- genes[GENE %in% cds_good$GENE]
 
@@ -334,11 +361,10 @@ nrow(cds_good)
 
 nrow(genes)
 nrow(genes_good)
-```
 
 ## Intergenic space and mappability
 ### Identify intergenic space
-```{r}
+
 intergenic <- genes_good[, .(
   TYPE = "intergenic",
   START = STOP + 1L,
@@ -350,10 +376,9 @@ intergenic <- intergenic[!is.na(STOP)]
 intergenic <- intergenic[START <= STOP]
 
 intergenic$ID<-1:nrow(intergenic)
-```
 
 ### Defining mappable regions
-```{r}
+
 # Genic space
 genes_good$ID<-1:nrow(genes_good)
 
@@ -367,95 +392,69 @@ intergenic$ID <- 1:nrow(intergenic)
 intergenic$LENGTH <- intergenic$STOP - intergenic$START
 
 mapp_genic <- features_in_features(intergenic, mapp[DEPTH==1], mode = "length")
-```
 
 ### Ratio of gene to intergenic
-```{r}
+
 sum(genes_good$LENGTH)/sum(intergenic$LENGTH)
-```
 
 ## Identify Homopolymers
 ### Generate Homopolymer data
-```{r, eval=FALSE}
+
 # Identify HPs in the genome
-HPs<-make_homopolymer(fasta, 3)
-fwrite(HPs,"./Data/Homopolymers/ref_chandler_primary_omnic.fasta.HPs.tsv", sep = "\t")
-```
+HPs <- make_homopolymer(fasta, 3)
+fwrite(HPs,"./ref_chandler_primary_omnic.fasta.HPs.tsv", sep = "\t")
 
 ### Read homopolymer data in
-```{r}
-HPs <- fread("./Data/Homopolymers/ref_chandler_primary_omnic.fasta.HPs.tsv")
-```
+
+#HPs <- fread("./Data/Homopolymers/ref_chandler_primary_omnic.fasta.HPs.tsv")
 
 # Mutations
 ## Preparing data
 ### Read data
-```{r}
+
 mutations <- fread("./Data/VCFs/Filtered/all_dn_snps.vcf")
 # mutations_ns <-fread("./Data/VCFs/Filtered/dn_snps_no_share.vcf")
-```
 
 ### Rename default column
-```{r}
+
 mutations <- mutations %>%
   rename("SAMPLE"= "default", "SOURCE" = "Source") %>%
   dplyr::select(-c(ID, INFO, Hap, unique))
 
-# mutations_ns <- mutations_ns %>%
-#   rename("SAMPLE" = "default") %>%
-#   select(-c(ID, INFO, Hap, unique))
-```
-
 ### Adjusting the unique column and identifying SNPs
-```{r}
+
 # Mutations
 mutations$UNIQUE <- paste(mutations$CHROM, mutations$POS, mutations$ALT, sep = "_")
 mutations[, NUMIND := .N, by = UNIQUE] # NUMIND column is the number of Sources with that mutation
 SNPS <- nchar(mutations$REF) == 1 & nchar(mutations$ALT) == 1
 mutations$SNP <- SNPS
 
-# No share mutations
-# mutations_ns$UNIQUE <- paste(mutations_ns$CHROM, mutations_ns$POS, mutations_ns$ALT, sep = "_")
-# mutations_ns[, N:=.N, by = UNIQUE]
-# SNPS_ns <- nchar(mutations_ns$REF)==1 & nchar(mutations_ns$ALT)==1
-# mutations_ns$SNP <- SNPS_ns
-```
-
 ## Annotate the mutations with CDS and Genic regions
 ### Annotating with CDS
-```{r}
+
 # Mutations
 mutations$ID <- 1:nrow(mutations)
 mutations_cds <- features_in_sites(cds_good, mutations)
 mutations$CDS <- mutations_cds$overlaps[match(mutations$ID, mutations_cds$ID)]
 
 table(mutations$CDS)
-# No share mutations
-# mutations_ns$ID<-1:nrow(mutations_ns)
-# mutations_ns_cds<-features_in_sites(cds_good, mutations_ns)
-# mutations_ns$CDS<-mutations_ns_cds$overlaps[match(mutations_ns$ID, mutations_ns_cds$ID)]
-```
 
 ### Annotating with genes
-```{r}
+
 # Mutations
 mutations_gene <- features_in_sites(genes_good, mutations)
 mutations$GENIC <- mutations_gene$overlaps[match(mutations$ID, mutations_gene$ID)]
 
 table(mutations$GENIC)
-# No share mutations
-# mutations_ns_gene<-features_in_sites(genes_good, mutations_ns)
-# mutations_ns$GENIC<-mutations_ns_gene$overlaps[match(mutations_ns$ID, mutations_ns_gene$ID)]
-```
 
 ## Split AD
-```{r}
+
 mutations$ALTDP <- sapply(mutations$AD, function(x) as.numeric(vstrsplit(x, split=",")[2]))
 mutations$REFDP <- sapply(mutations$AD, function(x) as.numeric(vstrsplit(x, split=",")[1]))
-```
+
 
 ## Calculation of deviation from heterozygous expection
-```{r, warning=FALSE}
+
 # Does the Chi square test warning matter?
 mutations$CHIPHET <- pbsapply(1:nrow(mutations), function(i) {
   
@@ -465,10 +464,9 @@ mutations$CHIPHET <- pbsapply(1:nrow(mutations), function(i) {
   
   return(chitest$p.value)
 })
-```
 
 ## Distribution of shared SNPs
-```{r}
+
 mutations %>%
   ggplot(aes(x = as.factor(NUMIND))) +
   geom_histogram(stat = "count") +
@@ -478,24 +476,21 @@ counts<-data.table(table(table(mutations$UNIQUE)))
 ggplot(counts, aes(x=as.numeric(V1), y=N))+
   geom_bar(stat="identity")+
   scale_y_log10()
-```
 
 ## Recalculating DP and VAF
 ### Calculate DP based on informative reads and normalize
-```{r}
+
 mutations$DPREAL <- mutations$ALTDP + mutations$REFDP
 
 mutations[, DPNORM:=log(DP/mean(DP)), by = SOURCE]
 mutations[, DPNORM_REAL:=log(DPREAL/mean(DPREAL)), by = SOURCE]
-```
 
 ### Calculate VAF based on informative reads
-```{r}
+
 mutations$VAFREAL <- mutations$ALTDP/mutations$DPREAL
-```
 
 ## Assign labels for samples
-```{r}
+
 mutations <- mutations %>%
   mutate(CULTURETYPE = case_when(
     SOURCE %in% c("ref_chandler", "tree2") ~ "tree",
@@ -508,10 +503,9 @@ mutations <- mutations %>%
     SOURCE %in% c("cr21_1", "cr21_2", "cr2_11A", "cr2_12A", "cr2_13A", "cr2_15A", "cr2_15A1", 
                   "cr2_16A", "cr2_16A1","cr2_16A2", "cr2_17A1", "cr2_17A2", "cr2_18A",
                   "cr2_18A1", "cr85", "cr10", "cr13", "cr22") ~ "short_read"))
-```
 
 ## Calculate summary stats for each SNP across all samples
-```{r}
+
 # Reset object as data table to avoid warnings
 mutations <- as.data.table(mutations)
 # Mean VAF
@@ -524,101 +518,58 @@ mutations[, medDPREAL:=median(DPREAL), by = UNIQUE]
 
 # Median Quality
 mutations[, medQUAL:=median(QUAL), by = UNIQUE]
-```
 
 ## Annotate mappability to means
-```{r}
+
 mutations_MAPP <- features_in_sites(mapp[DEPTH==1], mutations)
 mutations$MAPP <- mutations_MAPP$overlaps[match(mutations$ID, mutations_MAPP$ID)]
 table(mutations$MAPP, mutations$SEQTYPE)
-```
 
-<!-- ## Annotate mutations near homopolymeric regions -->
-<!-- ```{r} -->
-<!-- # Seems to be about 40 minutes -->
-<!-- mutations_neighbors <- rbindlist(pblapply((1:nrow(mutations)), function(i){ -->
-<!--   CHROM = mutations$CHROM[i] -->
-<!--   POS = mutations$POS[i] -->
-<!--   UNIQUE = mutations$UNIQUE[i] -->
-<!--   up = paste0(toupper(fasta[[CHROM]][(POS-10):(POS-1)]), collapse="") -->
-<!--   down = paste0(toupper(fasta[[CHROM]][(POS+1):(POS+10)]), collapse="") -->
-<!--  return(data.table(UNIQUE, up, down)) -->
-<!-- })) -->
+## Annotate mutations near homopolymeric regions 
+# Seems to be about 40 minutes 
+mutations_neighbors <- rbindlist(pblapply((1:nrow(mutations)), function(i){ 
+  CHROM = mutations$CHROM[i] 
+  POS = mutations$POS[i] 
+  UNIQUE = mutations$UNIQUE[i] 
+  up = paste0(toupper(fasta[[CHROM]][(POS-10):(POS-1)]), collapse="") 
+  down = paste0(toupper(fasta[[CHROM]][(POS+1):(POS+10)]), collapse="") 
+  return(data.table(UNIQUE, up, down)) 
+})) 
 
-<!-- mutations<-merge(mutations, mutations_neighbors, by="UNIQUE") -->
-<!-- ``` -->
+mutations <- merge(mutations, mutations_neighbors, by="UNIQUE") 
 
 ## Annotate trimer context for mutations
-```{r}
+
 # mutations contexts are only created for SNPs with this function, not INDELs. INDELS are filled with NAs.
 # Takes around 15 minutes
 mutations$TRICONTEXT <- tricontexts(mutations, fasta)
 mutations$MUT <- paste0(substr(mutations$TRICONTEXT, 2, 2), substr(mutations$TRICONTEXT, 4, 4), substr(mutations$TRICONTEXT, 6, 6))
 
 mutations$MUT[mutations$MUT == "NANANA"] <- NA
-```
 
-<!-- ## Calculate distance -->
-<!-- ```{r} -->
+## Create a means dataframe
 
-<!-- ## run post filter -->
-<!-- mutations_split<-split(mutations, by=c("CHROM","SOURCE")) -->
-<!-- mutations$DIST<- pbsapply(1:nrow(mutations), function(i){ -->
-<!--   C = mutations$CHROM[i] -->
-<!--   P = mutations$POS[i] -->
-<!--   S = mutations$SOURCE[i] -->
-<!--   listcoord<-paste0(C,".",S) -->
-<!--   other <- mutations_split[[listcoord]][POS!=P] -->
-<!--   min(abs(other$POS-P)) -->
-<!-- }) -->
-<!-- ``` -->
+mutations_means <- mutations[
+                           .(CHROM=unique(CHROM),
+                             POS=unique(POS),
+                             REF=unique(REF),
+                             ALT=unique(ALT),
+                             CDS=unique(CDS),
+                             GENIC=unique(GENIC)),
+                           by=.(UNIQUE)]
 
-## Organize mutations to save
-```{r}
-mutations_save <- mutations %>%
-  select(CHROM, POS, REF, ALT, UNIQUE, QUAL, FILTER, GT, GQ, REFDP, ALTDP, DPREAL, VAFREAL, DPNORM_REAL, medQUAL, medDPREAL, meanVAFREAL, SNP, GENIC, CDS, NUMIND, CULTURETYPE, SEQTYPE, MAPP, MUT, TRICONTEXT, SOURCE)
-```
+
+# annotate effects of mutations
+library(pbapply)
+mutations_means$effect<-pbsapply(1:nrow(mutations_means), annot_mut_effect)
+mutations_means$effect[mutations_means$effect=="nonCDS" & mutations_means$GENIC==T]<-"genic-nonCDS"
+
+# add back to mutations data
+mutations$effect<-mutations_means$effect[match(mutations$UNIQUE, mutations_means$UNIQUE)]
 
 # Write out new files
-```{r, eval=T}
-fwrite(mutations_save, "./Data/VCFs/Parsed/mutations.tsv", sep = "\t")
-fwrite(cds_good, "./Data/GFFs/Parsed/cds_good.tsv", sep = "\t")
-fwrite(genes_good, "./Data/GFFs/Parsed/genes_good.tsv", sep = "\t")
-```
+fwrite(mutations, "./mutations.tsv", sep = "\t")
+fwrite(cds_good, "./cds_good.tsv", sep = "\t")
+fwrite(genes_good, "./genes_good.tsv", sep = "\t")
 
-# Try to implement HPs
-## Clear environment to save memory
-```{r}
-rm(list = ls())
-```
-
-## Read in saved mutations and HPs
-```{r}
-muts <- fread("./Data/VCFs/Parsed/mutations.tsv")
-fasta <- read.fasta("./Data/Fastas/ref_chandler_primary_default_scaffold.fasta")
-HPs <- fread("./Data/Homopolymers/ref_chandler_primary_omnic.fasta.HPs.tsv")
-```
-
-## Select muts to make as small as possible
-```{r}
-muts_small <- muts %>%
-  select(CHROM, POS, UNIQUE)
-```
-
-
-## Run HPs
-## Annotate mutations near homopolymeric regions
-```{r}
-# Seems to be about 40 minutes
-mutations_neighbors <- rbindlist(pblapply((1:nrow(muts_small)), function(i){
-  CHROM = muts_small$CHROM[i]
-  POS = muts_small$POS[i]
-  UNIQUE = muts_small$UNIQUE[i]
-  up = paste0(toupper(fasta[[CHROM]][(POS-10):(POS-1)]), collapse="")
-  down = paste0(toupper(fasta[[CHROM]][(POS+1):(POS+10)]), collapse="")
- return(data.table(UNIQUE, up, down))
-}))
-
-muts_hp <- merge(muts_small, mutations_neighbors, by="UNIQUE")
-```
 
